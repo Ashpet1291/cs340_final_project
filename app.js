33489f1d6dcee5b67abdf32c8945ba8e9f4b3d79
Michael Uchmanowicz <muchm001@gmail.com>
2021-08-10 18:22:19 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 /*
     SETUP
-*/
+*/ 
     var express = require('express'); 
     var app = express();
     app.use(express.json())
@@ -18,7 +18,7 @@
             extname: ".hbs"
     }));
     app.set('view engine', '.hbs');
-    app.use(express.static(__dirname +'/public'));
+    app.use(express.static(__dirname +'/img'));
 /*
     ROUTES
 */

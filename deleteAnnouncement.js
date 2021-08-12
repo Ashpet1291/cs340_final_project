812b670e2d612d7d574108c56bbe68f029d43d47
muchm001@gmail.com <Ender1576!>
2021-08-11 20:07:01 -0500

    MJU

--- deleteAnnouncement.js
+++ deleteAnnouncement.js
@@ -1,2 +1,9 @@
-
-
+function deleteAnn(id){
+    $ajax({
+        url: '/announcements/' + id,
+        type: 'DELETE',
+        success: function(result){
+            window.location.reload(true);
+        }
+    })
+};
